f87afa12d6c7599e13f87b4268715327476793f7
snan3385 <snan3385@sysco.com>
2022-05-19 15:40:04 +0530

    sixth commit

---
+++ src/root.component.js
@@ -0,0 +1,10 @@
+import TeamOnboarding from "./TeamOnboarding";
+
+export default function Root(props) {
+  return (
+    <div>
+      {/*<section>{props.name} is mounted!</section>*/}
+      <TeamOnboarding />
+    </div>
+  );
+}
